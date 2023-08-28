fa0e7f933d50615d9c4b0d2469cffa5a73a564b2
Davi Gouveia <75867613+davitgouveia@users.noreply.github.com>
2023-08-28 11:26:23 -0300

    Adding login with Allauth

--- error_finances/error_finances/urls.py
+++ error_finances/error_finances/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('home.urls')),
+    path('accounts/', include('allauth.urls')),
     path('transactions/', include('transactions.urls')),
     path('admin/', admin.site.urls)
 ]
